2522a4dc670a7b0031cca76e0fecc741e547702a
BisonQin <bisonqin@gmail.com>
2017-10-04 00:05:10 +0800

    add stringToUnicode and stringToUTF8 utils in class EncodeUtils.java

--- app/src/main/java/cn/bisondev/myframework/utils/EncodeUtils.java
+++ app/src/main/java/cn/bisondev/myframework/utils/EncodeUtils.java
@@ -7,6 +7,8 @@ import android.util.Base64;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * 编码解码相关工具类
@@ -193,5 +195,169 @@ public final class EncodeUtils {
         } else {
             return Html.fromHtml(input);
         }
+    }
+
+    /**
+     * 中文字符串转为0x开头的十六进制Unicode码
+     * @param str 中文字符串
+     * @return 0x开头的十六进制Unicode码
+     */
+    public static String stringToUnicode(String str) {
+        try {
+            StringBuffer out = new StringBuffer("");
+            //直接获取字符串的unicode二进制
+            byte[] bytes = str.getBytes("unicode");
+            //然后将其byte转换成对应的16进制表示即可
+            for (int i = 0; i < bytes.length - 1; i += 2) {
+                out.append("0x");
+                String str1 = Integer.toHexString(bytes[i + 1] & 0xff);
+                for (int j = str1.length(); j < 2; j++) {
+                    out.append("0");
+                }
+                String str2 = Integer.toHexString(bytes[i] & 0xff);
+                out.append(str2);
+                out.append(str1);
+            }
+            return out.toString();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * 0x开头的十六进制Unicode码转为中文字符串
+     * @param str 0x开头的十六进制Unicode码字符串
+     * @return 中文字符
+     */
+    public static String unicodeToString(String str) {
+
+        Pattern pattern = Pattern.compile("(0x(\\p{XDigit}{4}))");
+        Matcher matcher = pattern.matcher(str);
+        char ch;
+        while (matcher.find()) {
+            String group = matcher.group(2);
+            ch = (char) Integer.parseInt(group, 16);
+            String group1 = matcher.group(1);
+            str = str.replace(group1, ch + "");
+        }
+        return str;
+    }
+
+    /**
+     * 中文字符串转为\\u开头的十六进制Unicode码
+     * @param str 中文字符串
+     * @return \\u开头的十六进制Unicode码
+     */
+    public static String stringToUnicode1(String str) {
+        try {
+            StringBuffer out = new StringBuffer("");
+            //直接获取字符串的unicode二进制
+            byte[] bytes = str.getBytes("unicode");
+            //然后将其byte转换成对应的16进制表示即可
+            for (int i = 0; i < bytes.length - 1; i += 2) {
+                out.append("\\u");
+                String str1 = Integer.toHexString(bytes[i + 1] & 0xff);
+                for (int j = str1.length(); j < 2; j++) {
+                    out.append("0");
+                }
+                String str2 = Integer.toHexString(bytes[i] & 0xff);
+                out.append(str2);
+                out.append(str1);
+            }
+            return out.toString();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * \\u开头的十六进制Unicode码转为中文字符串
+     * @param str \\u开头的十六进制Unicode码字符串
+     * @return 中文字符
+     */
+    public static String unicodeToString1(String str) {
+
+        Pattern pattern = Pattern.compile("(\\\\u(\\p{XDigit}{4}))");
+        Matcher matcher = pattern.matcher(str);
+        char ch;
+        while (matcher.find()) {
+            String group = matcher.group(2);
+            ch = (char) Integer.parseInt(group, 16);
+            String group1 = matcher.group(1);
+            str = str.replace(group1, ch + "");
+        }
+        return str;
+    }
+
+    /**
+     * 中文字符串转为UTF-8编码
+     * @param str 中文字符串（你）
+     * @return UTF-8编码（E4BDA0）
+     */
+    public static String convertStringToUTF8(String str) {
+        if (str == null || str.equals("")) {
+            return null;
+        }
+        StringBuffer sb = new StringBuffer();
+        try {
+            char c;
+            for (int i = 0; i < str.length(); i++) {
+                c = str.charAt(i);
+                if (c >= 0 && c <= 255) {
+                    sb.append(c);
+                } else {
+                    byte[] b;
+                    b = Character.toString(c).getBytes("utf-8");
+                    for (int j = 0; j < b.length; j++) {
+                        int k = b[j];
+                        //转换为unsigned integer  无符号integer
+                    /*if (k < 0)
+                        k += 256;*/
+                        k = k < 0? k+256:k;
+                        //返回整数参数的字符串表示形式 作为十六进制（base16）中的无符号整数
+                        //该值以十六进制（base16）转换为ASCII数字的字符串
+                        sb.append(Integer.toHexString(k).toUpperCase());
+
+                        // url转置形式
+                        // sb.append("%" +Integer.toHexString(k).toUpperCase());
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
+    }
+
+    /**
+     * UTF-8编码转为中文字符串
+     * @param str
+     * @return
+     */
+    public static String convertUTF8ToString(String str) {
+        if (str == null || str.equals("")) {
+            return null;
+        }
+        try {
+            str = str.toUpperCase();
+            int total = str.length() / 2;
+            //标识字节长度
+            int pos = 0;
+            byte[] buffer = new byte[total];
+            for (int i = 0; i < total; i++) {
+                int start = i * 2;
+                //将字符串参数解析为第二个参数指定的基数中的有符号整数。
+                buffer[i] = (byte) Integer.parseInt(str.substring(start, start + 2), 16);
+                pos++;
+            }
+            //通过使用指定的字符集解码指定的字节子阵列来构造一个新的字符串。
+            //新字符串的长度是字符集的函数，因此可能不等于子数组的长度。
+            return new String(buffer, 0, pos, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return str;
     }
 }
